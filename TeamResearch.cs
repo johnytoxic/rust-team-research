2e3d11ac859a86db5ecbd5003bb93d69324c58a0
johnytoxic <johnytoxic@protonmail.com>
2021-01-29 21:21:37 +0100

    implement team research sharing

--- TeamResearch.cs
+++ TeamResearch.cs
@@ -4,8 +4,23 @@
     [Description("Shares research across your team")]
     class TeamResearch : CovalencePlugin
     {
+        private RelationshipManager _relationshipManager;
         private void Init()
         {
+            _relationshipManager = RelationshipManager.Instance;
+        }
+
+        void OnTechTreeNodeUnlocked(Workbench workbench, TechTreeData.NodeInstance node, BasePlayer player)
+        {
+            var team = _relationshipManager.FindPlayersTeam(player.userID);
+            if (null != team)
+            {
+                foreach (ulong member in team.members)
+                {
+                    BasePlayer memberPlayer = BasePlayer.FindByID(member);
+                    memberPlayer.blueprints.Unlock(node.itemDef);
+                }
+            }
         }
     }
 }
