eb19e625b4c99fe17fdca1abe1799508fb2e9b59
johnytoxic <johnytoxic@protonmail.com>
2021-01-29 20:32:26 +0100

    create uMod plugin

--- Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace TeamResearch
-{
-    public class Class1
-    {
-    }
-}
---
+++ TeamResearch.cs
@@ -0,0 +1,11 @@
+﻿namespace Oxide.Plugins
+{
+    [Info("Team Research", "johnytoxic", "0.1.0")]
+    [Description("Shares research across your team")]
+    class TeamResearch : CovalencePlugin
+    {
+        private void Init()
+        {
+        }
+    }
+}
